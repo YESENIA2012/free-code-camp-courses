098fc9415af5e5901ea3cdc25f7cbea2e0d7a50a
YESENIA2012 <gyesenia173@gmail.com>
2022-02-28 17:20:44 -0500

    Function to get a property of an object

---
+++ phoneticLookup.js
@@ -0,0 +1,19 @@
+function phoneticLookup(val) {
+  let result = "";
+
+  // Only change code below this line
+  let lookup = {
+    alpha: "Adams",
+    bravo: "Boston",
+    charlie: "Chicago",
+    delta: "Denver",
+    echo: "Easy",
+    foxtrot: "Frank",
+  };
+  result = lookup[val];
+  // Only change code above this line
+  return result;
+}
+
+let resultado = phoneticLookup("charlie");
+console.log(resultado);
